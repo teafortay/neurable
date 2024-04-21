041cccaa4dcf02a7493f53a55944f98325e91b0a
Taylor Shaw <tay@taylorcodeshop.com>
2024-04-21 18:25:37 -0400

    added viewmodel

---
+++ Neurable/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  Neurable
+//
+//  Created by Taylor Shaw on 4/21/24.
+//
+
+import Foundation
+@MainActor
+class ContentViewModel: ObservableObject {
+    @Published var isOn = false
+}
