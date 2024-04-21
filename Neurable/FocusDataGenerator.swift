29342df47b146836918183ffac667925811526bc
Taylor Shaw <tay@taylorcodeshop.com>
2024-04-21 17:56:42 -0400

    added random number generator

---
+++ Neurable/FocusDataGenerator.swift
@@ -0,0 +1,25 @@
+//
+//  FocusDataGenerator.swift
+//  Neurable
+//
+//  Created by Taylor Shaw on 4/21/24.
+// let diceRoll = Int(arc4random_uniform(6) + 1)
+
+import Foundation
+
+struct Sample {
+var focusLevel: Float
+    var dataQuality: Float
+}
+
+func generateSample() -> Sample {
+    let focusLevel = Float(arc4random_uniform(100))
+    let dataQuality = Float(arc4random_uniform(100))
+  
+    return Sample(focusLevel: focusLevel, dataQuality: dataQuality)
+}
+
+func connectionIssue() -> Bool {
+    let value = Int(arc4random_uniform(100))
+    return value > 20
+}
