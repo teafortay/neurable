af8dc7579ae59d9ee1bd11161d5af818342fd104
Taylor Shaw <tay@taylorcodeshop.com>
2024-04-21 21:43:14 -0400

    drew line with hardcoded data

--- Neurable/ContentView.swift
+++ Neurable/ContentView.swift
@@ -6,6 +6,22 @@
 //
 
 import SwiftUI
+import Charts
+
+struct DataPoint: Identifiable {
+    var id = UUID()
+    
+    var focusLevel: Float
+    var timeInSec: Int
+}
+
+var data: [DataPoint] = [
+    DataPoint(focusLevel: 60.0, timeInSec: 1),
+    DataPoint(focusLevel: 46.5, timeInSec: 2),
+    DataPoint(focusLevel: 78.4, timeInSec: 3),
+    DataPoint(focusLevel: 31.2, timeInSec: 4),
+    DataPoint(focusLevel: 98.1, timeInSec: 5)
+]
 
 struct ContentView: View {
     @ObservedObject var viewmodel = ContentViewModel()
@@ -13,6 +29,15 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Neurable Demo")
+            Chart(data) {
+                    LineMark(
+                        x: .value("", $0.timeInSec),
+                        y: .value("", $0.focusLevel)
+                    )
+                    .foregroundStyle(.bar)
+                    .lineStyle(StrokeStyle(lineWidth: 10, lineJoin: .round))
+                }
+            
             Spacer()
             HStack {
                 Spacer()
