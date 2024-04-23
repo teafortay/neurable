f159b3bbe6dc8b7e0842ec156d7a8959ae1e6ae3
Taylor Shaw <tay@taylorcodeshop.com>
2024-04-23 00:42:56 -0400

    figured out how to use datasample and session objects

--- Neurable/ContentViewModel.swift
+++ Neurable/ContentViewModel.swift
@@ -18,36 +18,39 @@ class ContentViewModel: ObservableObject {
     @Published var isOn: Bool
     @Published var data: [DataPoint]
     var timer: Timer?
-    var seriesNum: Int
     
     init() {
         isOn = false
         data = []
-        seriesNum = 0
     }
     
     func toggleButton() {
         isOn.toggle()
         if isOn {
-            // start new line
-            seriesNum += 1
+            var sesh = FocusData_Session()
+            var seriesNum = 0
             var offSec = 0
             
             timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
                 let sample = generateSample()
                 let dropped = connectionIssue()
-                print(sample, !dropped, self.seriesNum)
+                print(sample, !dropped, seriesNum)
                 if sample.dataQuality > 30 && !dropped {
-                    self.data.append(DataPoint(series: self.seriesNum, focusLevel: sample.focusLevel, timeInSec: offSec))
+                    self.data.append(DataPoint(series: seriesNum, focusLevel: sample.focusLevel, timeInSec: offSec))
                 } else {
                     // when quality is bad or dropped
-                    self.seriesNum += 1
-                    
+                    seriesNum += 1
                 }
+                var dataSample = FocusData_Session.DataSample()
+                dataSample.offsetSeconds = Int32(offSec)
+                dataSample.dataQuality = sample.dataQuality
+                dataSample.focusLevel = sample.focusLevel
+                sesh.data.append(dataSample)
                 offSec += 1
             }
         } else {
             // TODO: create session data and validate, handle so don't resetsecs to 0
+            self.data = []
             timer?.invalidate()
             timer = nil
         }
